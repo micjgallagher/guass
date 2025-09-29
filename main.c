e08ea1b3fc3cbdb706d500dc5bc2b73e0fed7247
Michael Gallagher <micjgallagher@gmail.com>
2025-09-29 00:46:35 +0000

    Added row equivalency functions

--- main.c
+++ main.c
@@ -4,7 +4,14 @@
 #include "main.h"
 
 int main(int argc, char ** argv){
-    printf("Test\n");
+    Matrix m = createMatrix(3, 3);
+    setElement(&m, 10.2, 0, 0);
+    setElement(&m, 5.2, 0, 1);
+    setElement(&m, 2.52, 0, 2);
+    printMatrix(&m);
+    multiplyRow(&m, 0, 0.5);
+    printMatrix(&m);
+    destroyMatrix(&m);
     return 0;
 }
 
@@ -28,6 +35,9 @@ double getElement(Matrix *matrix, int r, int c){
 void setElement(Matrix *matrix, double value, int r, int c){
     matrix->body[getIndex(matrix, r, c)] = value;
 }
+void multiplyElement(Matrix *matrix, double coefficient, int r, int c){
+    setElement(matrix, getElement(matrix, r, c) * coefficient, r, c);
+}
 
 Matrix copyMatrix(Matrix *matrix){
     Matrix output;
@@ -43,3 +53,32 @@ void destroyMatrix(Matrix *matrix){
     matrix->rows = 0;
     matrix->rows = 0;
 }
+
+void reduceMatrix(Matrix *matrix){
+    //Bulk of the program will be in this function
+}
+
+void multiplyRow(Matrix *matrix, int row, double coefficient){
+   for (int i = 0; i < matrix->columns; i++) {
+        multiplyElement(matrix, coefficient, row, i);
+    }
+}
+
+void multiplyRowThenAdd(Matrix *matrix, int row_source, int row_destination, double coefficient){
+    for(int c=0; c<matrix->columns; c++){
+        double val = getElement(matrix, row_source, c) * coefficient + getElement(matrix, row_destination, c); 
+        setElement(matrix, val, row_destination, c);
+    }
+}
+
+
+void printMatrix(Matrix *matrix){
+    for(int r=0; r < matrix->rows;r++){
+        for(int c=0; c < matrix->columns;c++){
+           printf("%lf ", getElement(matrix, r, c)); 
+        }
+        printf("\n");
+    }
+}
+
+
--- main.h
+++ main.h
@@ -12,5 +12,14 @@ Matrix copyMatrix(Matrix * matrix);
 void destroyMatrix(Matrix * matrix);
 double getElement(Matrix * matrix, int r, int c);
 void setElement(Matrix * matrix, double value, int r, int c);
+void multiplyElement(Matrix * matrix, double coefficient, int r, int c);
 int getIndex(Matrix * matrix, int r, int c);
+void printMatrix(Matrix *matrix);
 
+//Gaussian Functions
+void reduceMatrix(Matrix * matrix);
+
+//Row Equivalency Functions
+
+void multiplyRow(Matrix * matrix, int row, double coefficient);
+void multiplyRowThenAdd(Matrix * matrix, int row_source, int row_destination, double coefficient);
